049422e4a60a790ba0d47faf7977605a1765d855
Angel Flores <cyberangel1000@gmail.com>
2021-02-27 16:12:33 -0600

    Started E25

---
+++ excercise_25/main.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    print("I'm going to guess for you a number. Think in a number between 1 and 100")
+    print("You will have to answer me [+] if is greater, [-] if is lower, or [=] if it is the right number.")
+
+
+    print(f"Your number is ? +/-/= ")
